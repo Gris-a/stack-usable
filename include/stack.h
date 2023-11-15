d6e08439f2209a8ce73a0bd38242c11f6c6585b1
Gris-a <zavialov.ga@phystech.edu>
2023-11-15 20:42:36 +0300

    Initial commit

---
+++ include/stack.h
@@ -0,0 +1,47 @@
+#ifndef STACK_H
+#define STACK_H
+
+#include "log.h"
+
+typedef long long data_t;
+#define DTS "%lld"
+
+struct Stack
+{
+    size_t size;
+    size_t capacity;
+
+    data_t *data;
+};
+
+#define STACK_DUMP(stack_ptr)   LOG("%s:%s:%d:\n", __FILE__, __PRETTY_FUNCTION__, __LINE__);\
+                                StackDump(stack_ptr)
+
+#ifdef PROTECT
+#define STACK_VER(stack_ptr, ret_val_on_fail)   if(StackVer(stack_ptr))\
+                                                {\
+                                                    LOG("%s:%s: Error: invalid stack.\n", __FILE__, __PRETTY_FUNCTION__);\
+                                                    STACK_DUMP(stack_ptr);\
+                                                    return ret_val_on_fail;\
+                                                }
+#else
+#define STACK_VER(...)
+#endif
+
+Stack StackCtor(const size_t capacity = 2);
+
+int StackDtor(Stack *stack);
+
+int PushStack(Stack *stack, const data_t val);
+
+int PopStack(Stack *stack, data_t *ret_val = NULL);
+
+int ClearStack(Stack *stack);
+
+void StackDump(Stack *stack);
+
+#ifdef PROTECT
+int StackVer(Stack *stack);
+#endif
+
+#endif //STACK_H
---
+++ source/stack.cpp
@@ -0,0 +1,131 @@
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+
+#include "../include/stack.h"
+
+Stack StackCtor(const size_t capacity)
+{
+    ASSERT(capacity, return {});
+
+    Stack stack = {};
+
+    stack.capacity = capacity;
+
+    stack.data = (data_t *)calloc(capacity * sizeof(data_t), sizeof(char));
+    ASSERT(stack.data, return {});
+
+    return stack;
+}
+
+int StackDtor(Stack *stack)
+{
+    STACK_VER(stack, EXIT_FAILURE);
+
+    stack->size     = ULLONG_MAX;
+    stack->capacity = 0;
+
+    free(stack->data);
+
+    return EXIT_SUCCESS;
+}
+
+
+static int OptimalExpansion(Stack *stack)
+{
+    if(stack->size == stack->capacity)
+    {
+        data_t *temp_ptr = (data_t *)realloc(stack->data, sizeof(data_t) * (stack->capacity *= 2));
+        ASSERT(temp_ptr, return EXIT_FAILURE);
+
+        stack->data = temp_ptr;
+
+        memset(stack->data + stack->size, 0, sizeof(data_t) * stack->size);
+    }
+
+    return EXIT_SUCCESS;
+}
+
+int PushStack(Stack *stack, const data_t val)
+{
+    STACK_VER(stack, EXIT_FAILURE);
+
+    EXEC_ASSERT(!OptimalExpansion(stack), return EXIT_FAILURE);
+
+    stack->data[stack->size++] = val;
+
+    return EXIT_SUCCESS;
+}
+
+
+static int OptimalShrink(Stack *stack)
+{
+    if(stack->size * 4 == stack->capacity)
+    {
+        data_t *temp_ptr = (data_t *)realloc(stack->data, sizeof(data_t) * (stack->capacity /= 2));
+        ASSERT(temp_ptr, return EXIT_FAILURE);
+
+        stack->data = temp_ptr;
+    }
+
+    return EXIT_SUCCESS;
+}
+
+int PopStack(Stack *stack, data_t *ret_val)
+{
+    STACK_VER(stack, EXIT_FAILURE);
+
+    ASSERT(stack->size != 0, return EXIT_FAILURE);
+
+    stack->size--;
+
+    if(ret_val) *ret_val = stack->data[stack->size];
+    stack->data[stack->size] = 0;
+
+    EXEC_ASSERT(!OptimalShrink(stack), return EXIT_FAILURE);
+
+    return EXIT_SUCCESS;
+}
+
+int ClearStack(Stack *stack)
+{
+    STACK_VER(stack, EXIT_FAILURE);
+
+    memset(stack->data, 0, sizeof(data_t) * stack->size);
+    stack->size = 0;
+
+    EXEC_ASSERT(!OptimalShrink(stack), return EXIT_FAILURE);
+
+    return EXIT_SUCCESS;
+}
+
+void StackDump(Stack *stack)
+{
+    ASSERT(stack, return);
+
+    LOG("Stack[%p]:         \n"
+        "\tsize     = %zu;  \n"
+        "\tcapacity = %zu;  \n"
+        "\tdata[%p]:        \n", stack, stack->size, stack->capacity, stack->data);
+
+    ASSERT(stack->data, return);
+
+    for(size_t i = 0; i < stack->capacity; i++)
+    {
+        LOG("\t\t");
+        LOG((i < stack->size) ? "*" : " ");
+        LOG("[%3zu] = " DTS "\n", i, stack->data[i]);
+    }
+}
+
+#ifdef PROTECT
+int StackVer(Stack *stack)
+{
+    ASSERT(stack      , return EXIT_FAILURE);
+    ASSERT(stack->data, return EXIT_FAILURE);
+
+    ASSERT(stack->capacity != 0 && stack->size <= stack->capacity, return EXIT_FAILURE);
+
+    return EXIT_SUCCESS;
+}
+#endif
